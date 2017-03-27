9821df189d8ab142ad1534ada0efe1c6867f2128
Rubén Martín Lozano <ruben.martin.lozano@gmail.com>
2017-03-27 16:32:11 +0200

    feat(cache): Use segmentation percentage instead of timing

--- src/decorators/cache/tracker/NodeTracker.js
+++ src/decorators/cache/tracker/NodeTracker.js
@@ -3,31 +3,16 @@ import http from 'http'
 import https from 'https'
 
 export default class NodeTracker extends Tracker {
-  constructor ({host, algorithm, protocol = 'http', period = 1000 * 20 /* 20 seconds */, env = Tracker.ENV_SERVER} = {}) {
-    super({host, algorithm, protocol, period, env})
-
-    this._host = host
-    this._algorithm = algorithm
-    this._protocol = protocol
-    this._period = period
-    this._env = env
-
-    this._resetStatsAndTimer()
-  }
-
   _send ({path, headers, port = 80, method = 'GET'} = {}) {
-    const client = this._protocol === 'http' ? http : https
-    client.get({
+    const request = this._protocol === 'http' ? http : https
+
+    request.get({
       hostname: this._host,
       port,
       path,
       headers
     })
 
-    this._resetStatsAndTimer()
-  }
-
-  _shouldSend () {
-    return Date.now() - this._timer > this._period
+    this._resetStats()
   }
 }
